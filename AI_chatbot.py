dbdb2a8d72a7d90aaa1d599b41257d93add61f19
sptryogi <115679084+sptryogi@users.noreply.github.com>
2025-05-26 16:30:49 +0700

    Update AI_chatbot.py

--- AI_chatbot.py
+++ AI_chatbot.py
@@ -55,9 +55,10 @@ def generate_text_deepseek(user_input, fitur, pasangan_cag, mode_bahasa="Sunda",
         Jangan memberikan informasi yang tidak tentu kebenarannya.
         Jawab pertanyaan secara sederhana saja jangan terlalu panjang dan jangan cerewet.
         Jangan gunakan huruf-huruf aneh seperti kanji korea, kanji jepang, atau kanji china.
-        Gunakan huruf pada awal kalimat dan setelah tanda titik.
+        Gunakan huruf kapital pada awal kalimat dan setelah tanda titik serta setelah petik dua.
         Gunakan huruf kapital pada awal nama orang dan nama tempat.
-        Jika disuruh atau diperintahkan untuk menterjemahkan atau translate, tolong jaga agar format paragraf dan barisnya tetap sama persis seperti teks asli atau input user.
+        Gunakan huruf kapital yang sama jika pada kalimat atau kata pada input user menggunakan huruf kapital.
+        Jika disuruh atau diperintahkan untuk menterjemahkan atau translate, tolong jaga agar format paragraf dan barisnya tetap sama persis seperti teks asli atau teks input user.
         Selalu akhiri dengan pertanyaan. 
         Pertanyaan dari pengguna: "{user_input}"
         """
@@ -66,13 +67,14 @@ def generate_text_deepseek(user_input, fitur, pasangan_cag, mode_bahasa="Sunda",
         final_prompt = f"""Kamu adalah penerjemah yang ahli bahasa sunda dan bahasa indonesia.
         Terjemahkan kalimat berikut ke dalam Bahasa Sunda LOMA secara alami seperti digunakan dalam kehidupan sehari-hari.
         Jaga agar format paragraf dan barisnya tetap sama persis seperti teks asli atau input user.
+        Gunakan huruf kapital yang sama jika pada kalimat atau kata pada input user menggunakan huruf kapital.
         Jangan mengajak mengobrol seperti fitur chatbot. anda hanya menterjemahkan input dari user seperti google translater.
         Jangan menambahkan kata bahasa sunda yang memang bukan arti dari kalimat bahasa indonesia tersebut.
         Sesuaikan gaya bahasanya agar cocok dengan konteks relasi antarpenutur dalam hal ini teman sebaya anak-anak umur 7 - 10 tahun.
         Perintah anda hanya terjemahkan dari input user, bukan menjawab hal lain. Jangan menggunakan kata awalan atau sapaan sebagai tambahan jawaban.
         Jangan beri penjelasan atau keterangan tambahan, langsung berikan hasil terjemahannya saja. 
         Jangan jadikan semua huruf pada awal kata huruf kapital kecuali nama tempat dan nama orang.
-        Huruf pada awal kalimat dan setelah titik harus huruf kapital.
+        Huruf pada awal kalimat dan setelah titik serta setelah petik dua harus huruf kapital.
         Nama orang dan nama tempat juga harus berawalan huruf kapital.
         Kalimat: {user_input}"""
         
@@ -80,10 +82,11 @@ def generate_text_deepseek(user_input, fitur, pasangan_cag, mode_bahasa="Sunda",
         final_prompt = f"""Kamu adalah penerjemah yang ahli bahasa indonesia dan bahasa sunda.
         Terjemahkan kalimat berikut ke dalam Bahasa Indonesia yang baku dan mudah dimengerti.
         Jaga agar format paragraf dan barisnya tetap sama persis seperti teks asli atau input user.
+        Gunakan huruf kapital yang sama jika pada kalimat atau kata pada input user menggunakan huruf kapital.
         Jangan mengajak mengobrol seperti fitur chatbot, anda hanya menterjemahkan input dari user seperti google translate.
         Jangan tambahkan penjelasan atau keterangan apa pun. Langsung tampilkan hasil terjemahannya.
         Jangan jadikan semua huruf pada awal kata huruf kapital, kecuali nama orang dan nama tempat.
-        Huruf pada awal kalimat dan setelah titik harus huruf kapital.
+        Huruf pada awal kalimat dan setelah titik serta setelah petik dua harus huruf kapital.
         Nama orang dan nama tempat juga harus berawalan huruf kapital.
         Kalimat: {user_input}"""
 
