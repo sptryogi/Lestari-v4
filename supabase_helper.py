6b1e2e55c37c156afee3fd21ef65e1ac1567d6ac
sptryogi <115679084+sptryogi@users.noreply.github.com>
2025-05-28 10:50:22 +0700

    Update supabase_helper.py

--- supabase_helper.py
+++ supabase_helper.py
@@ -8,7 +8,7 @@ SUPABASE_KEY = st.secrets["SUPABASE_KEY"]
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 
 def sign_in_with_email(email, password):
-    return supabase.auth.sign_in_with_password({"email": email, "password": password})
+    return supabase.auth.sign_in_with_password({"email": email, "umur":umur, "password": password})
 
 def sign_out():
     supabase.auth.sign_out()
