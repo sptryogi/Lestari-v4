fd6c8de9e9a8239a5c009c6cccf7c613be0ff7f7
sptryogi <115679084+sptryogi@users.noreply.github.com>
2025-05-28 09:38:24 +0700

    Update supabase_helper.py

--- supabase_helper.py
+++ supabase_helper.py
@@ -1,13 +1,17 @@
 from supabase import create_client, Client
-import os
+import streamlit as st
 
-SUPABASE_URL = os.getenv("SUPABASE_URL", "https://your-project.supabase.co")
-SUPABASE_KEY = os.getenv("SUPABASE_KEY", "your-service-role-key")
+# Ambil dari st.secrets
+SUPABASE_URL = st.secrets["SUPABASE_URL"]
+SUPABASE_KEY = st.secrets["SUPABASE_KEY"]
 
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 
 def sign_in_with_email(email, password):
     return supabase.auth.sign_in_with_password({"email": email, "password": password})
+
+def sign_out():
+    supabase.auth.sign_out()
 
 def get_user_session():
     return supabase.auth.get_session()
