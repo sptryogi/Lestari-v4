62380bef5ecc593c6dd59b8c91f00f10b8445ff9
sptryogi <115679084+sptryogi@users.noreply.github.com>
2025-05-28 11:10:53 +0700

    Update supabase_helper.py

--- supabase_helper.py
+++ supabase_helper.py
@@ -1,14 +1,19 @@
 from supabase import create_client, Client
 import streamlit as st
 
-# Ambil dari st.secrets
 SUPABASE_URL = st.secrets["SUPABASE_URL"]
 SUPABASE_KEY = st.secrets["SUPABASE_KEY"]
 
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 
-def sign_in_with_email(email, umur, password):
-    return supabase.auth.sign_in_with_password({"email": email, "umur":umur, "password": password})
+def sign_in_with_email(email, password):
+    return supabase.auth.sign_in_with_password({"email": email, "password": password})
+
+def sign_up_with_email(email, password, age):
+    result = supabase.auth.sign_up({"email": email, "password": password})
+    user_id = result.user.id
+    supabase.table("profiles").insert({"id": user_id, "age": age}).execute()
+    return result
 
 def sign_out():
     supabase.auth.sign_out()
@@ -16,13 +21,19 @@ def sign_out():
 def get_user_session():
     return supabase.auth.get_session()
 
-def insert_chat_history(user_id, message, response):
+def insert_chat_history(user_id, room, message, response):
     supabase.table("chat_history").insert({
         "user_id": user_id,
+        "room": room,
         "message": message,
         "response": response
     }).execute()
 
-def fetch_chat_history(user_id):
-    result = supabase.table("chat_history").select("*").eq("user_id", user_id).order("created_at", desc=False).execute()
+def fetch_chat_history(user_id, room):
+    result = supabase.table("chat_history") \
+        .select("*") \
+        .eq("user_id", user_id) \
+        .eq("room", room) \
+        .order("created_at", desc=False) \
+        .execute()
     return result.data if result else []
