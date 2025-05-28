7921dfad1c8bf0472b9cec4b15ee180863638ef7
Syahmi33github <78089958+Syahmi33github@users.noreply.github.com>
2025-05-28 16:32:54 +0700

    Update test_get_age.py

--- pages/test_get_age.py
+++ pages/test_get_age.py
@@ -1,5 +1,5 @@
 from AI_chatbot import get_age_by_email, get_age_by_id
 import streamlit as st
 
-age = get_age_by_id(st.session_state["email"])
+age = get_age_by_id(st.session_state.get("user"))
 st.text(age)
