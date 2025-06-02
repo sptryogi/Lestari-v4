965069cd0e2e59098ccd307ec749a3a4e163c363
sptryogi <115679084+sptryogi@users.noreply.github.com>
2025-06-02 13:50:56 +0700

    Update login.py

--- pages/login.py
+++ pages/login.py
@@ -256,11 +256,11 @@ def chat_ui():
 if "logout" in st.query_params:
     sign_out()
     st.session_state.clear()
-    # st.rerun()
+    st.rerun()
     
-if "user" in st.session_state and "logout" not in st.query_params:
-    with st.spinner("Mengarahkan ke halaman utama..."):
-        st.switch_page("main.py")
+# if "user" in st.session_state and "logout" not in st.query_params:
+#     with st.spinner("Mengarahkan ke halaman utama..."):
+#         st.switch_page("main.py")
 
 if st.session_state.get("user"):
     with st.spinner("Mengarahkan ke halaman utama..."):
