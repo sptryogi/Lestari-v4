826fa370a5966c95c73a8e2f483ea7ab0284ae71
sptryogi <115679084+sptryogi@users.noreply.github.com>
2025-06-10 10:16:52 +0700

    Update main.py

--- main.py
+++ main.py
@@ -398,7 +398,8 @@ def handle_send():
     if fitur == "chatbot" and mode_bahasa == "Sunda":
         bot_response = generate_text_deepseek(user_input, fitur, pasangan_cag, mode_bahasa, chat_mode, history=history_for_prompt)
         pasangan_ganti_ekuivalen = {}
-        text_constraint, kata_terdapat, kata_tidak_terdapat, pasangan_kata, pasangan_ekuivalen = highlight_text(bot_response, df_kamus, df_idiom, fitur)
+        bot_response_ekuivalen = ubah_ke_lema(bot_response, df_kamus, df_idiom)
+        text_constraint, kata_terdapat, kata_tidak_terdapat, pasangan_kata, pasangan_ekuivalen = highlight_text(bot_response_ekuivalen, df_kamus, df_idiom, fitur)
         text_constraint = kapitalisasi_awal_kalimat(text_constraint)
     elif fitur == "chatbot" and (mode_bahasa == "Indonesia" or mode_bahasa == "English"):
         bot_response = generate_text_deepseek(user_input, fitur, pasangan_cag, mode_bahasa, chat_mode, history=history_for_prompt)
